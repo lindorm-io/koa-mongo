6f6b0aa5fada2c141ae4406083f852bf043aca92
Jonn Nilsson <jonn@jonn.nu>
2021-05-29 08:59:06 +0200

    fix: update strict typing

--- src/middleware/mongo-middleware.ts
+++ src/middleware/mongo-middleware.ts
@@ -1,24 +1,23 @@
+import { DefaultState, Middleware } from "koa";
 import { IKoaMongoContext } from "../types";
 import { IMongoConnectionOptions, MongoConnection } from "@lindorm-io/mongo";
-import { Middleware, Next } from "koa";
 
-export const mongoMiddleware = (options: IMongoConnectionOptions): Middleware => async (
-  ctx: IKoaMongoContext,
-  next: Next,
-): Promise<void> => {
-  const start = Date.now();
+export const mongoMiddleware =
+  (options: IMongoConnectionOptions): Middleware<DefaultState, IKoaMongoContext> =>
+  async (ctx, next): Promise<void> => {
+    const start = Date.now();
 
-  ctx.client.mongo = new MongoConnection(options);
+    ctx.client.mongo = new MongoConnection(options);
 
-  await ctx.client.mongo.connect();
+    await ctx.client.mongo.connect();
 
-  ctx.logger.debug("mongo connection established");
+    ctx.logger.debug("mongo connection established");
 
-  ctx.metrics.mongo = (ctx.metrics.mongo || 0) + (Date.now() - start);
+    ctx.metrics.mongo = (ctx.metrics.mongo || 0) + (Date.now() - start);
 
-  try {
-    await next();
-  } finally {
-    await ctx.client.mongo.disconnect();
-  }
-};
+    try {
+      await next();
+    } finally {
+      await ctx.client.mongo.disconnect();
+    }
+  };
--- src/middleware/repository-middleware.ts
+++ src/middleware/repository-middleware.ts
@@ -1,5 +1,5 @@
+import { DefaultState, Middleware } from "koa";
 import { IKoaMongoContext } from "../types";
-import { Middleware, Next } from "koa";
 import { MongoRepository } from "@lindorm-io/mongo";
 import { camelCase } from "lodash";
 
@@ -7,23 +7,25 @@ interface RepositoryMiddlewareOptions {
   key?: string;
 }
 
-export const repositoryMiddleware = (
-  Repository: typeof MongoRepository,
-  options?: RepositoryMiddlewareOptions,
-): Middleware => async (ctx: IKoaMongoContext, next: Next): Promise<void> => {
-  const start = Date.now();
+export const repositoryMiddleware =
+  (
+    Repository: typeof MongoRepository,
+    options?: RepositoryMiddlewareOptions,
+  ): Middleware<DefaultState, IKoaMongoContext> =>
+  async (ctx, next): Promise<void> => {
+    const start = Date.now();
 
-  /*
-   * Ignoring TS here since Repository needs to be abstract
-   * to ensure that all input at least attempts to be unique
-   */
-  // @ts-ignore
-  ctx.repository[camelCase(options?.key || Repository.name)] = new Repository({
-    db: await ctx.client.mongo.database(),
-    logger: ctx.logger,
-  });
+    /*
+     * Ignoring TS here since Repository needs to be abstract
+     * to ensure that all input at least attempts to be unique
+     */
+    // @ts-ignore
+    ctx.repository[camelCase(options?.key || Repository.name)] = new Repository({
+      db: await ctx.client.mongo.database(),
+      logger: ctx.logger,
+    });
 
-  ctx.metrics.repository = (ctx.metrics.repository || 0) + (Date.now() - start);
+    ctx.metrics.repository = (ctx.metrics.repository || 0) + (Date.now() - start);
 
-  await next();
-};
+    await next();
+  };
